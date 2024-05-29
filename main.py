9aa1d7a592216762d75d8bf78692b0583f553f25
Samcoding5854 <samcoding5854@gmail.com>
2024-05-30 02:03:08 +0530

    chages

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import cv2
 import time
-import subprocess
+import pyautogui
 from HandInfoDetector import HandInfo
 from Utils import EmotionDetector
 from cvzone.HandTrackingModule import HandDetector
 import webbrowser
 
-
 ############################################################
-
 
 cap = cv2.VideoCapture(0)
 wCam, hCam = 640, 480
 cap.set(3, wCam)
 cap.set(4, hCam)
 
-tipIds = [4, 8, 12, 16, 20] #Finger Tips Id for Mediapipe
+tipIds = [4, 8, 12, 16, 20]  # Finger Tips Id for Mediapipe
 
 detectorInd = HandDetector(detectionCon=0.8, maxHands=2)
 
@@ -49,10 +47,9 @@ def convert_annotations(current_annotations):
 
     return converted_annotations
 
-
 while True:
     success, img = cap.read()
-    emotion = EmotionDetector(img) # Emotion Detector 
+    emotion = EmotionDetector(img)  # Emotion Detector
     print(emotion)
     hands, img = detectorInd.findHands(img)
 
@@ -85,10 +82,7 @@ while True:
             lmlist = hand1["lmList"]
             print("lmlist: ", lmlist)
 
-
-
             ### Counting the Number Displayed on Hand ###
-
             if lmlist[tipIds[0]][1] > lmlist[tipIds[0] - 1][1]:
                 fingers.append(1)
             else:
@@ -107,19 +101,19 @@ while True:
             ### Running Commands for next / previous / play / pause ###
             if totalFingers == 1:
                 if fingers == [0, 0, 0, 0, 1]:
-                    subprocess.Popen('nircmd sendkeypress mediaplaypause', shell=True)  # Play/Pause Track
-                    print("Play/Pause Track")
+                    pyautogui.press('nexttrack')  # Next Track
+                    print("Next Track")
 
                 elif fingers == [1, 0, 0, 0, 0]:
-                    subprocess.Popen('nircmd sendkeypress prevtrack', shell=True)  # Previous Track
+                    pyautogui.press('prevtrack')  # Previous Track
                     print("Previous Track Played")
 
             elif totalFingers == 0:
-                subprocess.Popen('nircmd sendkeypress mediaplaypause', shell=True)  # Pause
+                pyautogui.press('playpause')  # Pause
                 print("Paused")
 
             elif totalFingers == 5:
-                subprocess.Popen('nircmd sendkeypress play', shell=True)  # Play
+                pyautogui.press('play')  # Play
                 print("Play")
 
         ### Showing the FPS ###
