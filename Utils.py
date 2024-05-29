1ec58fb47e9ce905d8074db0af0ed878e5462f23
Samcoding5854 <samcoding5854@gmail.com>
2024-05-29 23:37:12 +0530

    Final changes

--- Utils.py
+++ Utils.py
@@ -1,129 +1,15 @@
-import os
 import tensorflow as tf
 import numpy as np
-from keras.layers import Layer
-import random
-from keras.models import Model
-from keras.layers import Layer,Conv2D, Dense, MaxPooling2D, Input, Flatten
 import cv2
 from keras.models import model_from_json
 
-## Face Rec ##
-
-class L1Dist(Layer):
-    
-    # Init method - inheritance
-    def __init__(self, **kwargs):
-        super().__init__()
-       
-    # Magic happens here - similarity calculation
-    def call(self, input_embedding, validation_embedding):
-        return tf.math.abs(input_embedding - validation_embedding)
-
-
-
-def preprocess(file_path):
-    
-    # Read in image from file path
-    byte_img = tf.io.read_file(file_path)
-    # Load in the image 
-    img = tf.io.decode_jpeg(byte_img)
-    
-    # Preprocessing steps - resizing the image to be 100x100x3
-    img = tf.image.resize(img, (100,100))
-    # Scale image to be between 0 and 1 
-    img = img / 255.0
-
-    # Return image
-    return img
-
-
-def verify(model, detection_threshold, verification_threshold):
-    results = []
-    
-
-    for image in os.listdir('FacePics/imagemyvalid'):
-        input_img = preprocess('FacePics/INput/input_image.jpg')
-        validation_img = preprocess(os.path.join('FacePics/imagemyvalid/', image))
-        # print(validation_img)
-
-        result = model.predict(list(np.expand_dims([input_img, validation_img], axis=1)))
-        results.append(result)
-
-    detection = np.sum(np.array(results) > detection_threshold)
-        
-    # Verification Threshold: Proportion of positive predictions / total positive samples 
-    verification = detection / 104 
-    verified = verification >  verification_threshold
-    return results,verified
-
-def make_embedding(): 
-    inp = Input(shape=(100,100,3), name='input_image')
-    
-    # First block
-    c1 = Conv2D(64, (10,10), activation='relu')(inp)
-    m1 = MaxPooling2D(64, (2,2), padding='same')(c1)
-    
-    # Second block
-    c2 = Conv2D(128, (7,7), activation='relu')(m1)
-    m2 = MaxPooling2D(64, (2,2), padding='same')(c2)
-    
-    # Third block 
-    c3 = Conv2D(128, (4,4), activation='relu')(m2)
-    m3 = MaxPooling2D(64, (2,2), padding='same')(c3)
-    
-    # Final embedding block
-    c4 = Conv2D(256, (4,4), activation='relu')(m3)
-    f1 = Flatten()(c4)
-    d1 = Dense(4096, activation='sigmoid')(f1)
-    
-    
-    return Model(inputs=[inp], outputs=[d1], name='embedding')
-
-def make_siamese_model():
-    
-    embedding = make_embedding()
-
-    input_image = Input(name = "input_img", shape=(100,100,3))
-    validation_image = Input(name = "validation_img", shape=(100,100,3))
-    
-    siamese_layer = L1Dist()
-    siamese_layer._name = 'distance'
-    distances = siamese_layer(embedding(input_image), embedding(validation_image)) 
-    
-    classifier = Dense(1,activation='sigmoid')(distances)
-    
-    return Model(inputs=[input_image, validation_image], outputs = classifier, name='SiameseNetwork') 
-
-
-## Game Control ##
-
-MIN_NUMBER = 1
-MAX_NUMBER = 5
-user_score = 0
-computer_score = 0  # Initialize computer_score
-
-def generate_computer_number():
-    return random.randint(MIN_NUMBER, MAX_NUMBER)
-
-def play_inning(user_input):
-    global user_score, computer_score  # Include computer_score in global scope
-
-    computer_number = generate_computer_number()
-
-    print("Computer's number:", computer_number)
-
-    if user_input < MIN_NUMBER or user_input > MAX_NUMBER:
-        print("Invalid input. Choose a number between 1 and 5.")
-        return
-
-    if user_input == computer_number:
-        print("You're out!")
-    else:
-        user_score += user_input
-        print("Your current score:", user_score)
-
 ## Emotion Detector ##
+
+def extract_features(image):
+    feature = np.array(image)
+    feature = feature.reshape(1,48,48,1)
+    return feature/255.0
+
 
 json_file = open("emotiondetectornew1.json", "r")
 model_json = json_file.read()
@@ -134,10 +20,6 @@ model.load_weights("emotiondetectornew1.h5")
 haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade=cv2.CascadeClassifier(haar_file)
 
-def extract_features(image):
-    feature = np.array(image)
-    feature = feature.reshape(1,48,48,1)
-    return feature/255.0
 
 labels = {0 : 'angry', 1 : 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'neutral', 5 : 'sad', 6 : 'surprise'}
 def EmotionDetector(im):
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import cv2 
+import cv2
 import time
 import subprocess
 from HandInfoDetector import HandInfo
 from Utils import EmotionDetector
 from cvzone.HandTrackingModule import HandDetector
+import webbrowser
+import pyautogui
 
 
 ############################################################
@@ -14,13 +16,9 @@ wCam, hCam = 640, 480
 cap.set(3, wCam)
 cap.set(4, hCam)
 
-
-
 tipIds = [4, 8, 12, 16, 20] #Finger Tips Id for Mediapipe
 
-
 detectorInd = HandDetector(detectionCon=0.8, maxHands=2)
-
 
 link_opened = False
 cTime = 0
@@ -29,11 +27,11 @@ tracking_duration = 60
 current_emotion = None
 pTime = 0
 emotion_actions = {
-    "happy": "xdg-open ##INSERT THE PLAYLIST LINK##",
-    "sad": "xdg-open ##INSERT THE PLAYLIST LINK##",
-    "angry": "xdg-open ##INSERT THE PLAYLIST LINK##",
-    "neutral": "xdg-open ##INSERT THE PLAYLIST LINK##",
-    "shocked": "xdg-open ##INSERT THE PLAYLIST LINK##"
+    "happy": "##INSERT THE PLAYLIST LINK##",
+    "sad": "##INSERT THE PLAYLIST LINK##",
+    "angry": "##INSERT THE PLAYLIST LINK##",
+    "neutral": "##INSERT THE PLAYLIST LINK##",
+    "shocked": "##INSERT THE PLAYLIST LINK##"
 }
 
 #############################################################
@@ -42,7 +40,7 @@ while True:
     success, img = cap.read()
     emotion = EmotionDetector(img) #Emotion Detector 
     print(emotion)
-    hands, img =detectorInd.findHands(img)
+    hands, img = detectorInd.findHands(img)
 
     if emotion in emotion_actions:
         if current_emotion != emotion:
@@ -52,7 +50,7 @@ while True:
         elapsed_time = time.time() - start_time
         if elapsed_time >= tracking_duration and not link_opened:
             action = emotion_actions[current_emotion]
-            subprocess.Popen(action, shell=True)
+            webbrowser.open(action)  # Open the URL in the default web browser
             link_opened = True
             current_emotion = None
 
@@ -60,18 +58,17 @@ while True:
         current_emotion = None
         start_time = None
 
-    numberOfHands= HandInfo(hands)
-    
-
+    numberOfHands = HandInfo(hands)
 
     if len(hands) != 0:
-    
-        print(f' Number Of Hands: {numberOfHands}')           
+        print(f' Number Of Hands: {numberOfHands}')
         ### Play Pause Activation Code ###
         if numberOfHands == '1':
             fingers = []
             hand1 = hands[0]
+            print("hand1: ", hand1)
             lmlist = hand1["lmList"]
+            print("lmlist: ", lmlist)
 
             ### Counting the Number Displayed on Hand ###
 
@@ -80,44 +77,39 @@ while True:
             else:
                 fingers.append(0)
 
-            for id in range(1,5):
-                if lmlist[tipIds[id]][2] < lmlist[tipIds[id]-2][2]: #Tips number's Y-Position if less than the 2nd point of the same finger, its closed and vice-versa
+            for id in range(1, 5):
+                if lmlist[tipIds[id]][2] < lmlist[tipIds[id] - 2][2]:  # Tips number's Y-Position if less than the 2nd point of the same finger, it's closed and vice-versa
                     fingers.append(1)
-                
                 else:
                     fingers.append(0)
 
-   
             totalFingers = fingers.count(1)
-       
+
             print(f'Number of Fingers : {totalFingers}')
 
             ### Running Commands for next / previous / play / pause ###
             if totalFingers == 1:
                 if fingers == [0, 0, 0, 0, 1]:
-                    subprocess.Popen('playerctl --all-players next', shell=True)
+                    pyautogui.hotkey('ctrl', 'right')  # Next track
                     print("Next Track Played")
-            
-                elif fingers == [1, 0, 0, 0, 0]:
-                        subprocess.Popen('playerctl --all-players previous', shell=True)
-                        print("Previous Track Played")
 
+                elif fingers == [1, 0, 0, 0, 0]:
+                    pyautogui.hotkey('ctrl', 'left')  # Previous track
+                    print("Previous Track Played")
 
             elif totalFingers == 0:
-                subprocess.Popen('playerctl --all-players pause', shell=True)
+                pyautogui.hotkey('playpause')  # Pause
                 print("Paused")
-            
+
             elif totalFingers == 5:
-                subprocess.Popen('playerctl --all-players play', shell=True)
+                pyautogui.hotkey('playpause')  # Play
                 print("Play")
 
         ### Showing the FPS ###
         cTime = time.time()
-        fps = 1/(cTime - pTime)
+        fps = 1 / (cTime - pTime)
         pTime = cTime
-        cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_COMPLEX, 3, (255,0,255), 3)
-
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
 
     cv2.imshow("IMAGE", img)
     cv2.waitKey(1)
-
